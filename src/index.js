dea2ff981f72dae0596fb13c4fb428f99d3069d5
Finch31 <jenya87@inbox.ru>
2023-01-27 15:57:45 +0300

    feat: update reverse-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+    let isNegative = false;
+    if (n < 0) {
+        isNegative = true;
+        n = -n;
+    }
+    let reverse = 0;
+    while (n > 0) {
+        reverse = reverse * 10 + n % 10;
+        n = parseInt(n / 10);
+    }
+    if (reverse >= Math.pow(2, 31) - 1 || reverse <= Math.pow(-2, 31)) {
+        return 0;
+    }
+    return isNegative ? reverse : reverse;
+};
